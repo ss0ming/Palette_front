d1c7270020791c1addc128c36cb408c4e9969b0d
seorinnn <srgongju@naver.com>
2024-01-06 18:01:39 +0900

    240106

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,17 @@
+import styles from "./HomePage.module.css";
+
 function HomePage() {
   return (
     <>
-      <p>나는 홈페이지당!</p>
+      <div className={styles.home}>
+        <header className={styles.homeHeader}>
+          <img alt="homeImg" src={require(`../assets/homeImg.PNG`)}></img>
+        </header>
+        <main className={styles.main}>
+          <h3>현재 구인중인 프로젝트들</h3>
+          <button>왼쪽 버튼</button>
+        </main>
+      </div>
     </>
   );
 }
--- src/pages/ProjectInformationPage.js
+++ src/pages/ProjectInformationPage.js
@@ -1,54 +1,80 @@
+import { useState } from "react";
 import styles from "./ProjectInformationPage.module.css";
+import heartBeforeImg from "../assets/heartBefore.png";
+import heartAfterImg from "../assets/heartAfter.png";
 
 function ProjectInformation() {
+  const [imageSrc, setImageSrc] = useState(heartBeforeImg);
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    if (isClicked) {
+      setImageSrc(heartBeforeImg);
+      setIsClicked(false); //초기 상태로
+    } else {
+      setImageSrc(heartAfterImg);
+      setIsClicked(true);
+    }
+  };
+
   return (
     <>
-      <div className={styles.header}>
-        <img alt="projectImg" src={require(`../assets/project_img.jpg`)} />
-        <div className={styles.headerInfo}>
-          <h1>프로젝트명</h1>
-          <p>사용자 정보</p>
+      <div className={styles.projectInformation}>
+        <div className={styles.recruitmentStatus}>
+          <p>모집중</p>
         </div>
-      </div>
-      <div className={styles.main}>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>프로젝트 소개 및 기간</h3>
-          <hr />
-          <div className={styles.content}>
-            <p>기간 : 2023.12 ~ 2024. 06</p>
-            <p>이 프로젝트는 ~ 어쩌구저쩌구</p>
+        <header className={styles.projectInfoHeader}>
+          <img alt="projectImg" src={require(`../assets/project_img.jpg`)} />
+          <div className={styles.headerInfo}>
+            <div className={styles.title}>
+              <h1>프로젝트명</h1>
+              <img alt="heart" src={imageSrc} onClick={handleClick} />
+            </div>
+            <p>사용자 정보</p>
           </div>
-        </div>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>현재 모집 현황</h3>
-          <hr />
-          <div className={styles.content}>
-            <p>웹 프론트엔드 0/1</p>
+        </header>
+        <main className={styles.projectInfoMain}>
+          <div className={styles.section}>
+            <h3 className={styles.sectionTitle}>프로젝트 소개 및 기간</h3>
+            <hr />
+            <div className={styles.content}>
+              <p>기간 : 2023.12 ~ 2024. 06</p>
+              <p>이 프로젝트는 ~ 어쩌구저쩌구</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>기술</h3>
-          <hr />
-          <div className={styles.content}>
-            <p>기술 이미지</p>
+          <div className={styles.section}>
+            <h3 className={styles.sectionTitle}>현재 모집 현황</h3>
+            <hr />
+            <div className={styles.content}>
+              <p>웹 프론트엔드 0/1</p>
+              <p>웹 서버 0/1</p>
+              <p>UI/UX 디자인 0/1</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>참고 링크</h3>
-          <hr />
-          <div className={styles.content}>
-            <p>
-              https://www.notion.so/f7147a5a76cd4c698ef6bb84c68dbcd0?v=7fbb4ce09edf489291de3fdf2b70ca24
-            </p>
+          <div className={styles.section}>
+            <h3 className={styles.sectionTitle}>기술</h3>
+            <hr />
+            <div className={styles.content}>
+              <p>기술 이미지</p>
+            </div>
           </div>
-        </div>
-        <div className={styles.section}>
-          <h3 className={styles.sectionTitle}>댓글</h3>
-          <hr />
-          <div className={styles.content}>
-            <p>어쩌구저쩌구 댓글~</p>
+          <div className={styles.section}>
+            <h3 className={styles.sectionTitle}>참고 링크</h3>
+            <hr />
+            <div className={styles.content}>
+              <a href="https://www.notion.so/f7147a5a76cd4c698ef6bb84c68dbcd0?v=7fbb4ce09edf489291de3fdf2b70ca24">
+                https://www.notion.so/f7147a5a76cd4c698ef6bb84c68dbcd0?v=7fbb4ce09edf489291de3fdf2b70ca24
+              </a>
+            </div>
           </div>
-        </div>
+          <div className={styles.section}>
+            <h3 className={styles.sectionTitle}>댓글</h3>
+            <hr />
+            <div className={styles.content}>
+              <p>어쩌구저쩌구 댓글~</p>
+            </div>
+          </div>
+        </main>
       </div>
     </>
   );
