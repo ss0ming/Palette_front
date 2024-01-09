ce1b743d58a78304341e3c18d6acad5aac1ec872
woohyun1007 <kwh001007@naver.com>
2024-01-09 17:58:43 +0900

    240109

--- src/pages/ProjectListPage.js
+++ src/pages/ProjectListPage.js
@@ -1,19 +1,20 @@
 import styles from "./ProjectListPage.module.css";
 import ProjectCategory from "../components/ProjectCategory.js";
+import PopularUserBlank from "../components/PopularUserBlank.js";
 
 function ProjectListPage() {
   return (
     <section>
-      <div className={styles.row}>
+      <div style={{ display: 'flex', flex: '1', flexDirection: 'row', justifyContent: 'space-evenly' }}>
         <div><h1 className={styles.mainletter}>신규 프로젝트</h1>
-        <ProjectCategory/>
+          <ProjectCategory />
         </div>
         <div><h1 className={styles.mainletter}>관심 많은 프로젝트</h1>
-        <ProjectCategory/>
+          <ProjectCategory />
         </div>
       </div>
       <div><h1 className={styles.mainletter}>인기있는 사용자</h1>
-      <ProjectCategory/>
+        <PopularUserBlank />
       </div>
     </section>
   );
