0468846d787d14766717780a4012618137da92b6
seorinnn <srgongju@naver.com>
2024-02-02 17:53:40 +0900

    fix : merge 완료 후 수정 시도

--- src/components/PopularUser.js
+++ src/components/PopularUser.js
@@ -1,26 +1,24 @@
 //eslint-disable-next-line
 import { Link, NavLink } from "react-router-dom";
 import styles from "./PopularUser.module.css";
-import jsondata from "../api/mock.json"
+import jsondata from "../api/mock.json";
 
 function PopularUser() {
   const data = jsondata;
   const p = data.projects;
-  
-  return (
-    p.map(project => (
-      <Link to="/ProjectInformation">
-        <div key={project.projectId}>
-          <img
-            className={styles.photo}
-            alt="img"
-            src={require(`../assets/${project.image}`)}
-          />
-          <h1 className={styles.mainletter}>{project.publisher}</h1>
-        </div>
-      </Link>
-    ))
-  );
+
+  return p.map((project) => (
+    <Link to={`/projectInformation/${project.slug}`}>
+      <div key={project.projectId}>
+        <img
+          className={styles.photo}
+          alt="img"
+          src={require(`../assets/${project.image}`)}
+        />
+        <h1 className={styles.mainletter}>{project.publisher}</h1>
+      </div>
+    </Link>
+  ));
 }
 
 export default PopularUser;
--- src/components/Project.js
+++ src/components/Project.js
@@ -1,39 +1,37 @@
 //eslint-disable-next-line
 import { Link, NavLink } from "react-router-dom";
-import styles from "./Project.module.css"
-import jsondata from "../api/mock.json"
+import styles from "./Project.module.css";
+import jsondata from "../api/mock.json";
 
 function Project() {
   const data = jsondata;
   const p = data.projects;
 
-  return (
-    p.map(project => (
-      <Link to="/ProjectInformation">
-        <div key={project.projectId} style={{ width: '100%', height: '100%', position: 'relative' }}>
-          <div style={{ width: 680, height: 150, left: 0, top: 0, position: 'relative', background: 'white', borderRadius: 20, border: '0.20px rgba(0, 0, 0, 0.50) solid' }} />
-          <div style={{ width: 100, height: 100, left: 25, top: 12, position: 'absolute' }}>
-            <img
-              className={styles.photo}
-              alt="img"
-              src={require(`../assets/${project.image}`)}
-            />
-          </div>
-          <div style={{ width: 470, height: 25, left: 150, top: 11, position: 'absolute', color: 'black', fontSize: 20, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>{project.title}</div>
-          <div style={{ width: 470, height: 69, left: 150, top: 43, position: 'absolute', color: 'rgba(0, 0, 0, 0.50)', fontSize: 15, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>{project.introduction}</div>
-          <div style={{ width: 20, height: 20, left: 25, top: 124, position: 'absolute', background: '#CCCCCC' }}></div>
-          <div style={{ width: 20, height: 20, left: 86, top: 124, position: 'absolute' }}>
-            <div style={{ width: 20, height: 20, left: 0, top: 0, position: 'absolute' }}>
-              <div style={{ width: 20, height: 20, left: 0, top: 0, position: 'absolute' }}></div>
-              <div style={{ width: 18, height: 13, left: 0, top: 0, position: 'absolute', background: '#CCCCCC' }}></div>
-            </div>
-          </div>
-          <div style={{ width: 180, height: 20, left: 150, top: 123, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>작성자 : {project.publisher}</div>
-          <div style={{ width: 250, height: 20, left: 430, top: 123, position: 'absolute', color: 'black', fontSize: 16, fontFamily: 'Inter', fontWeight: '400', wordWrap: 'break-word' }}>최근 업데이트 날짜 : {project.date}</div>
+  return p.map((project) => (
+    <Link to={`/projectInformation/${project.slug}`}>
+      <div key={project.projectId}>
+        <div />
+        <div>
+          <img
+            className={styles.photo}
+            alt="img"
+            src={require(`../assets/${project.image}`)}
+          />
         </div>
-      </Link >
-    ))
-  );
+        <div>{project.title}</div>
+        <div>{project.introduction}</div>
+        <div></div>
+        <div>
+          <div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <div>작성자 : {project.publisher}</div>
+        <div>최근 업데이트 날짜 : {project.date}</div>
+      </div>
+    </Link>
+  ));
 }
 
 export default Project;
